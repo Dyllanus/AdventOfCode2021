fd63b9c30cbb64ffab8d345e39e87cf0224f8cb6
Dyllanus <74240279+Dyllanus@users.noreply.github.com>
2021-12-01 15:34:42 +0100

    wow

---
+++ src/AdventOfCode2020/day2/Day2Opdr1.java
@@ -0,0 +1,42 @@
+package AdventOfCode2020.day2;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+public class Day2Opdr1 {
+    public static void main(String[] args) {
+        int valid = 0;
+        int laagCijfer, hoogCijfer = 0;
+        char letter;
+        try {
+            BufferedReader reader =  new BufferedReader(new FileReader("src/AdventOfCode2020/day2/input.txt"));
+            String line = reader.readLine();
+            while(line !=null){
+                String[] splitst = line.split(": ");
+                String password = splitst[1];
+                splitst = splitst[0].split(" ");
+
+                letter = splitst[1].charAt(0);
+
+                laagCijfer = Integer.parseInt(splitst[0].split("-")[0]);
+                hoogCijfer = Integer.parseInt(splitst[0].split("-")[1]);
+
+                int counterChar = 0;
+                for (char c: password.toCharArray()){
+                    if (c==letter){
+                        counterChar++;
+                    }
+                }
+                if (counterChar >= laagCijfer && counterChar <=hoogCijfer){
+                    valid++;
+                }
+
+                line = reader.readLine();
+            }
+            System.out.println(valid);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
