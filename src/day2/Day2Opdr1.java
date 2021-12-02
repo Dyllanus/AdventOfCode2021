c6fd3d4ff22b6d1643162ba4603a4b4fd46c3dc6
Dyllanus <74240279+Dyllanus@users.noreply.github.com>
2021-12-02 11:34:48 +0100

    day 2 completed

--- src/day2/Day2Opdr1.java
+++ src/day2/Day2Opdr1.java
@@ -6,18 +6,50 @@ public class Day2Opdr1 {
     public static void main(String[] args) {
         String path = "src/day2/input.txt";
         List<String> strings = Utils.readFileToStrings(path);
-        List<Integer> integers = Utils.readFileToIntegers(path);
 
-        opdracht1(strings, integers);
-        opdracht2(strings, integers);
+
+//        opdracht1(strings);
+        opdracht2(strings);
     }
 
-    private static void opdracht1(List<String> strings, List<Integer> integers){
-
+    private static void opdracht1(List<String> strings){
+        int depth = 0;
+        int hor = 0;
+        for (String regel: strings){
+            String direction = regel.split(" ")[0];
+            int hoeveel = Integer.parseInt(regel.split(" ")[1]);
+            switch (direction){
+                case "up":
+                    depth-=hoeveel;
+                    break;
+                case "down":
+                    depth+=hoeveel;
+                    break;
+                case "forward":
+                    hor+=hoeveel;
+                    break;
+            }
+        };
+        System.out.println(hor* depth);
     }
 
-    private static void opdracht2(List<String> strings, List<Integer> integers){
-
+    private static void opdracht2(List<String> strings){
+        int depth = 0;
+        int hor = 0;
+        int aim= 0;
+        for (String regel: strings){
+            String direction = regel.split(" ")[0];
+            int hoeveel = Integer.parseInt(regel.split(" ")[1]);
+            switch (direction) {
+                case "up" -> aim -= hoeveel;
+                case "down" -> aim += hoeveel;
+                case "forward" -> {
+                    hor += hoeveel;
+                    depth += hoeveel * aim;
+                }
+            }
+        };
+        System.out.println(hor* depth);
     }
 
 }
